59ea1d45e842938255da061f7ca44662e75d4db7
grigorovaa <megrimarik13@gmail.com>
2023-07-23 00:56:45 +0300

    solitions to c04

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgrigoro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 11:47:55 by mgrigoro          #+#    #+#             */
+/*   Updated: 2023/07/12 13:21:38 by mgrigoro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb == -2147483648)
+	{
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + 48);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + 48);
+	}
+}
+
+/*
+int	main(void)
+{
+	int	num;
+
+	num = 42;
+	ft_putnbr(num);
+	return (0);
+}
+*/
